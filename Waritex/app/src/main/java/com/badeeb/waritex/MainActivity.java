b327d364587d1babe3ce85f47247de0282723e2e
alghawy2010 <alghawy2010@gmail.com>
2017-06-07 00:43:58 +0300

    V1.3
    Empty tabs are created and tested in emulator

--- Waritex/app/src/main/java/com/badeeb/waritex/MainActivity.java
+++ Waritex/app/src/main/java/com/badeeb/waritex/MainActivity.java
@@ -1,13 +1,58 @@
 package com.badeeb.waritex;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.badeeb.waritex.adapter.ViewPagerAdapter;
+import com.badeeb.waritex.fragment.ActivePromotionsFragment;
+import com.badeeb.waritex.fragment.ExpiredPromotionsFragment;
+import com.badeeb.waritex.fragment.ProductsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Logging Purpose
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    // Main Fragment attributes
+    private Toolbar mtoolbar;
+    private TabLayout mtabLayout;
+    private ViewPager mviewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate - Start");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Toolbar
+        this.mtoolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        this.setSupportActionBar(this.mtoolbar);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // ViewPager
+        this.mviewPager = (ViewPager) this.findViewById(R.id.viewpager);
+        this.setupViewPager(this.mviewPager);   // Defines the number of tabs by setting appropriate fragment and tab name
+
+        // Tabs
+        this.mtabLayout = (TabLayout) this.findViewById(R.id.tabs);
+        this.mtabLayout.setupWithViewPager(this.mviewPager);        // Assigns the ViewPager to TabLayout.
+
+        Log.d(TAG, "onCreate - End");
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new ProductsFragment(), getString(R.string.products_tab_title));
+        adapter.addFragment(new ActivePromotionsFragment(), getString(R.string.active_promotions_tab_title));
+        adapter.addFragment(new ExpiredPromotionsFragment(), getString(R.string.expired_promotions_tab_title));
+
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ Waritex/app/src/main/java/com/badeeb/waritex/adapter/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.badeeb.waritex.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Amr Alghawy on 6/7/2017.
+ * Custom adapter class provides fragments required for the view pager.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    // Define class attributes
+    private final List<Fragment> mFragmentList;
+    private final List<String> mFragmentTitleList;
+
+    // Constructor
+    public ViewPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+
+        // Attributes initialization
+        this.mFragmentList = new ArrayList<>();
+        this.mFragmentTitleList = new ArrayList<>();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return this.mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return this.mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        return this.mFragmentTitleList.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+}
