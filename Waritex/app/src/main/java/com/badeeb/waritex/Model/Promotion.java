b3d6ca623a0be4683569c2d998dd7a30442a21d6
amahgoub <ahmed.saleh.mahgoub@gmail.com>
2017-06-07 00:46:31 +0300

    add promotion fragment

---
+++ Waritex/app/src/main/java/com/badeeb/waritex/Model/Promotion.java
@@ -0,0 +1,23 @@
+package com.badeeb.waritex.Model;
+
+/**
+ * Created by ahmed on 6/7/2017.
+ */
+
+public class Promotion {
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
