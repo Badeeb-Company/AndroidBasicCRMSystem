a8937dba91cc3695a520ff4a3f0e1c117b0899d7
amahgoub <ahmed.saleh.mahgoub@gmail.com>
2017-06-07 23:43:28 +0300

    v1.6
    adding recycler view in expired fragment

--- Waritex/app/src/main/java/com/badeeb/waritex/fragment/ActivePromotionsFragment.java
+++ Waritex/app/src/main/java/com/badeeb/waritex/fragment/ActivePromotionsFragment.java
@@ -15,7 +15,8 @@ import android.view.ViewGroup;
 
 import com.badeeb.waritex.model.Promotion;
 import com.badeeb.waritex.R;
-import com.badeeb.waritex.adapter.ActivePromotionsAdaptor;
+import com.badeeb.waritex.adapter.PromotionsRecyclerViewAdaptor;
+import com.badeeb.waritex.view.GridSpacingItemDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,14 +27,14 @@ import java.util.List;
 public class ActivePromotionsFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
-    private ActivePromotionsAdaptor mAdapter;
+    private PromotionsRecyclerViewAdaptor mAdapter;
     private List<Promotion> mPromotionList;
-    private int promotionPerLine;
+    private int mPromotionPerLine;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        promotionPerLine = 2;
+        mPromotionPerLine = 2;
 
     }
 
@@ -67,9 +68,9 @@ public class ActivePromotionsFragment extends Fragment {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.active_promotion_recycler_view);
 
         mPromotionList = new ArrayList<Promotion>();
-        mAdapter = new ActivePromotionsAdaptor(getActivity(), mPromotionList);
+        mAdapter = new PromotionsRecyclerViewAdaptor(getActivity(), mPromotionList);
 
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), promotionPerLine);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), mPromotionPerLine);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -78,44 +79,6 @@ public class ActivePromotionsFragment extends Fragment {
         preparePromotions();
 
         return view;
-    }
-//
-    /**
-     * RecyclerView item decoration - give equal margin around grid item
-     */
-    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
-                }
-            }
-        }
     }
 
     /**
--- Waritex/app/src/main/java/com/badeeb/waritex/fragment/ExpiredPromotionsFragment.java
+++ Waritex/app/src/main/java/com/badeeb/waritex/fragment/ExpiredPromotionsFragment.java
@@ -1,30 +1,92 @@
 package com.badeeb.waritex.fragment;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.badeeb.waritex.R;
+import com.badeeb.waritex.adapter.PromotionsRecyclerViewAdaptor;
+import com.badeeb.waritex.model.Promotion;
+import com.badeeb.waritex.view.GridSpacingItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ExpiredPromotionsFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private PromotionsRecyclerViewAdaptor mAdapter;
+    private List<Promotion> mPromotionList;
+    private int mPromotionPerLine;
 
     public ExpiredPromotionsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPromotionPerLine = 2;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expired_promotions, container, false);
+        View view =  inflater.inflate(R.layout.fragment_expired_promotions, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.expired_promotion_recycler_view);
+
+        mPromotionList = new ArrayList<Promotion>();
+        mAdapter = new PromotionsRecyclerViewAdaptor(getActivity(), mPromotionList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), mPromotionPerLine);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+
+        preparePromotions();
+
+        return view;
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+
+    private void preparePromotions() {
+        int[] photos = {
+                R.drawable.photo1,
+                R.drawable.photo2,
+                R.drawable.photo3,
+                R.drawable.photo4,
+                R.drawable.photo5
+
+        };
+        for (int i = 0 ; i< photos.length ; i++){
+            Promotion promo = new Promotion();
+            promo.setDueDateFormated("1"+i+"-Jan-2017");
+            promo.setTitle("Promotion Title "+i+1);
+            promo.setMainPhoto(photos[i]+"");
+            mPromotionList.add(promo);
+        }
+        mAdapter.notifyDataSetChanged();
     }
 
 }
