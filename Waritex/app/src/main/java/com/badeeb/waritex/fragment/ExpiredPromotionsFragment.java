9be06180ed19a9d348b9e6ea661d9b9b2b9c3d8d
amahgoub <ahmed.saleh.mahgoub@gmail.com>
2017-06-08 01:09:40 +0300

    v1.9
    dimesions refactor

--- Waritex/app/src/main/java/com/badeeb/waritex/fragment/ExpiredPromotionsFragment.java
+++ Waritex/app/src/main/java/com/badeeb/waritex/fragment/ExpiredPromotionsFragment.java
@@ -43,7 +43,7 @@ public class ExpiredPromotionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+//        // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_expired_promotions, container, false);
         mRecyclerView = (RecyclerView) view.findViewById(R.id.expired_promotion_recycler_view);
 
